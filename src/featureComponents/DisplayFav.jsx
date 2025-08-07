8460255c8eab56e9bc4d205da3360a46796816df
yensg <yen.compound@gmail.com>
2025-08-07 21:52:26 +0800

    is Loading

--- src/featureComponents/DisplayFav.jsx
+++ src/featureComponents/DisplayFav.jsx
@@ -171,7 +171,7 @@ const DisplayFav = () => {
         </div>
       </div>
       <div className={`container gap-2`}>
-        {queryAirTable.isLoading && "Is loading..."}
+        {queryAirTable.isLoading && "is loading..."}
         {queryAirTable.isSuccess &&
           queryAirTable.data.records.map((item) => {
             return (
